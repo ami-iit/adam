ac676a3a6b4ae5255c92df03590deeeaf29fd9f1
Giuseppe L'Erario <gl.giuseppelerario@gmail.com>
2021-06-08 11:42:32 +0200

    Merge devel in main (#1)
    Implemented the first version of the module

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # ADAM
-Automatic Differentiation for rigid body dynamics algorithMs
+
+**Automatic Differentiation for rigid-body-dynamics AlgorithMs**
+
+This library implements kinematics and dynamics algorithms for **floating-base** robots, in _mixed representation_ (see [Traversaro's A Unified View of the Equations of Motion used for Control Design of Humanoid Robots](https://www.researchgate.net/publication/312200239_A_Unified_View_of_the_Equations_of_Motion_used_for_Control_Design_of_Humanoid_Robots)).
+
+ADAM employs [CasADi](https://web.casadi.org/), which embeds the computed kinematics and dynamics quantities in expression graphs and provides if needed, gradients, Jacobians, and Hessians. This approach enables the design of optimal control strategies in robotics. Using its `CodeGenerator`, CasADi enables also the generation of C-code - usable also in `Matlab` or `C++`.
+
+Adam is based on Roy Featherstone's Rigid Body Dynamics Algorithms.
+
+This work is still at an early stage and bugs could jump out!
+PRs are welcome! :rocket:
+
+## :hammer: Dependencies
+
+- [`python3`](https://wiki.python.org/moin/BeginnersGuide)
+
+Other requisites are:
+
+- `urdf_parser_py`
+- `casadi`
+
+They will be installed in the installation step!
+
+## :floppy_disk: Installation
+
+Install `python3`, if not installed (in **Ubuntu 20.04**):
+
+```bash
+sudo apt install python3.8
+```
+
+Clone the repo and install the library:
+
+```bash
+git clone https://github.com/dic-iit/ADAM.git
+cd ADAM
+pip install .
+```
+
+preferably in a [virtual environment](https://docs.python.org/3/library/venv.html#venv-def). For example:
+
+```bash
+pip install virtualenv
+python3 -m venv your_virtual_env
+source your_virtual_env/bin/activate
+```
+
+## :rocket: Usage
+
+```python
+from adam.core.computations import KinDynComputations
+import gym_ignition_models
+import numpy as np
+
+# if you want to use gym-ignition https://github.com/robotology/gym-ignition to retrieve the urdf
+model_path = gym_ignition_models.get_model_file("iCubGazeboV2_5")
+# The joint list
+joints_name_list = [
+    'torso_pitch', 'torso_roll', 'torso_yaw', 'l_shoulder_pitch',
+    'l_shoulder_roll', 'l_shoulder_yaw', 'l_elbow', 'r_shoulder_pitch',
+    'r_shoulder_roll', 'r_shoulder_yaw', 'r_elbow', 'l_hip_pitch', 'l_hip_roll',
+    'l_hip_yaw', 'l_knee', 'l_ankle_pitch', 'l_ankle_roll', 'r_hip_pitch',
+    'r_hip_roll', 'r_hip_yaw', 'r_knee', 'r_ankle_pitch', 'r_ankle_roll'
+]
+# Specify the root link
+root_link = 'root_link'
+kinDyn = KinDynComputations(urdf_path, joints_name_list, root_link)
+w_H_b = np.eye(4)
+joints = np.ones(len(joints_name_list))
+M = kinDyn.mass_matrix_fun()
+print(M(w_H_b, joints))
+```
+
+## Todo
+
+- [x] Center of Mass position
+- [x] Jacobians
+- [x] Forward kinematics
+- [x] Mass Matrix via CRBA
+- [x] Centroidal Momentum Matrix via CRBA
+- [ ] Recursive Newton-Euler algorithm
+- [ ] Articulated Body algorithm
+
+---
+
+The structure of the library is inspired by the module [urdf2casadi](https://github.com/mahaarbo/urdf2casadi/blob/master/README.md), which generates kinematic and dynamics quantities using CasADi, for Fixed-Base robots. Please check their interesting work!
---
+++ tests/test_computations.py
@@ -0,0 +1,161 @@
+import logging
+
+import casadi as cs
+import gym_ignition_models
+import idyntree.swig as idyntree
+import numpy as np
+import pytest
+
+from adam.core.computations import KinDynComputations
+from adam.geometry import utils
+
+model_path = gym_ignition_models.get_model_file("iCubGazeboV2_5")
+
+joints_name_list = [
+    "torso_pitch",
+    "torso_roll",
+    "torso_yaw",
+    "l_shoulder_pitch",
+    "l_shoulder_roll",
+    "l_shoulder_yaw",
+    "l_elbow",
+    "r_shoulder_pitch",
+    "r_shoulder_roll",
+    "r_shoulder_yaw",
+    "r_elbow",
+    "l_hip_pitch",
+    "l_hip_roll",
+    "l_hip_yaw",
+    "l_knee",
+    "l_ankle_pitch",
+    "l_ankle_roll",
+    "r_hip_pitch",
+    "r_hip_roll",
+    "r_hip_yaw",
+    "r_knee",
+    "r_ankle_pitch",
+    "r_ankle_roll",
+]
+
+
+def SX2DM(x):
+    return cs.DM(x)
+
+
+def H_from_PosRPY_idyn(xyz, rpy):
+    T = idyntree.Transform.Identity()
+    R = idyntree.Rotation.RPY(rpy[0], rpy[1], rpy[2])
+    p = idyntree.Position()
+    [p.setVal(i, xyz[i]) for i in range(3)]
+    T.setRotation(R)
+    T.setPosition(p)
+    return T
+
+
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("Showing the robot tree.")
+
+root_link = "root_link"
+comp = KinDynComputations(model_path, joints_name_list, root_link)
+robot_iDyn = idyntree.ModelLoader()
+robot_iDyn.loadReducedModelFromFile(model_path, joints_name_list)
+
+kinDyn = idyntree.KinDynComputations()
+kinDyn.loadRobotModel(robot_iDyn.model())
+kinDyn.setFloatingBase(root_link)
+kinDyn.setFrameVelocityRepresentation(idyntree.MIXED_REPRESENTATION)
+n_dofs = len(joints_name_list)
+
+xyz = (np.random.rand(3) - 0.5) * 5
+rpy = (np.random.rand(3) - 0.5) * 5
+
+H_b_idyn = H_from_PosRPY_idyn(xyz, rpy)
+vb = idyntree.Twist()
+vb.zero()
+s = idyntree.VectorDynSize(n_dofs)
+joints_val = (np.random.rand(n_dofs) - 0.5) * 5
+[s.setVal(i, joints_val[i]) for i in range(n_dofs)]
+s_dot = idyntree.VectorDynSize(n_dofs)
+[s_dot.setVal(i, 0) for i in range(n_dofs)]
+g = idyntree.Vector3()
+g.zero()
+kinDyn.setRobotState(H_b_idyn, s, vb, s_dot, g)
+H_b = utils.H_from_PosRPY(xyz, rpy)
+s_ = joints_val
+
+
+def test_mass_matrix():
+    M = comp.mass_matrix_fun()
+    mass_mx = idyntree.MatrixDynSize()
+    kinDyn.getFreeFloatingMassMatrix(mass_mx)
+    mass_mxNumpy = mass_mx.toNumPy()
+    mass_test = SX2DM(M(H_b, s_))
+    assert mass_test - mass_mxNumpy == pytest.approx(0.0, abs=1e-5)
+
+
+def test_CMM():
+    Jcm = comp.centroidal_momentum_matrix_fun()
+    cmm_idyntree = idyntree.MatrixDynSize()
+    kinDyn.getCentroidalTotalMomentumJacobian(cmm_idyntree)
+    cmm_idyntreeNumpy = cmm_idyntree.toNumPy()
+    Jcm_test = SX2DM(Jcm(H_b, s_))
+    assert Jcm_test - cmm_idyntreeNumpy == pytest.approx(0.0, abs=1e-5)
+
+
+def test_CoM_pos():
+    com_f = comp.CoM_position_fun()
+    CoM_cs = SX2DM(com_f(H_b, s_))
+    CoM_iDynTree = kinDyn.getCenterOfMassPosition().toNumPy()
+    assert CoM_cs - CoM_iDynTree == pytest.approx(0.0, abs=1e-5)
+
+
+def test_total_mass():
+    assert comp.get_total_mass() - robot_iDyn.model().getTotalMass() == pytest.approx(
+        0.0, abs=1e-5
+    )
+
+
+def test_jacobian():
+    J_tot = comp.jacobian_fun("l_sole")
+    iDyntreeJ_ = idyntree.MatrixDynSize(6, n_dofs + 6)
+    kinDyn.getFrameFreeFloatingJacobian("l_sole", iDyntreeJ_)
+    iDynNumpyJ_ = iDyntreeJ_.toNumPy()
+    s_ = joints_val
+    H_b = utils.H_from_PosRPY(xyz, rpy)
+    J_test = SX2DM(J_tot(H_b, s_))
+    assert iDynNumpyJ_ - J_test == pytest.approx(0.0, abs=1e-5)
+
+
+def test_jacobian_non_actuated():
+    J_tot = comp.jacobian_fun("head")
+    iDyntreeJ_ = idyntree.MatrixDynSize(6, n_dofs + 6)
+    kinDyn.getFrameFreeFloatingJacobian("head", iDyntreeJ_)
+    iDynNumpyJ_ = iDyntreeJ_.toNumPy()
+    s_ = joints_val
+    H_b = utils.H_from_PosRPY(xyz, rpy)
+    J_test = SX2DM(J_tot(H_b, s_))
+    assert iDynNumpyJ_ - J_test == pytest.approx(0.0, abs=1e-5)
+
+
+def test_fk():
+    H_idyntree = kinDyn.getWorldTransform("l_sole")
+    p_idy2np = H_idyntree.getPosition().toNumPy()
+    R_idy2np = H_idyntree.getRotation().toNumPy()
+    T = comp.forward_kinematics_fun("l_sole")
+    s_ = joints_val
+    H_b = utils.H_from_PosRPY(xyz, rpy)
+    H_test = SX2DM(T(H_b, s_))
+    assert R_idy2np - H_test[:3, :3] == pytest.approx(0.0, abs=1e-5)
+    assert p_idy2np - H_test[:3, 3] == pytest.approx(0.0, abs=1e-5)
+
+
+def test_fk_non_actuated():
+    H_idyntree = kinDyn.getWorldTransform("head")
+    p_idy2np = H_idyntree.getPosition().toNumPy()
+    R_idy2np = H_idyntree.getRotation().toNumPy()
+    T = comp.forward_kinematics_fun("head")
+    s_ = joints_val
+    H_b = utils.H_from_PosRPY(xyz, rpy)
+    H_test = SX2DM(T(H_b, s_))
+    assert R_idy2np - H_test[:3, :3] == pytest.approx(0.0, abs=1e-5)
+    assert p_idy2np - H_test[:3, 3] == pytest.approx(0.0, abs=1e-5)
