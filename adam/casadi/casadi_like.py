1e07bd834ef88d14ccfb89d25eef104f9d9131fa
Giuseppe <gl.giuseppelerario@gmail.com>
2022-03-25 12:03:49 +0100

    More annotations

--- adam/casadi/casadi_like.py
+++ adam/casadi/casadi_like.py
@@ -16,70 +16,86 @@ class CasadiLike(ArrayLike):
     array: Union[cs.SX, cs.DM]
 
     def __matmul__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides @ operator"""
         if type(self) is type(other):
             return CasadiLike(self.array @ other.array)
         else:
             return CasadiLike(self.array @ other)
 
     def __rmatmul__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides @ operator"""
         if type(self) is type(other):
             return CasadiLike(other.array @ self.array)
         else:
             return CasadiLike(other @ self.array)
 
     def __mul__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides * operator"""
         if type(self) is type(other):
             return CasadiLike(self.array * other.array)
         else:
             return CasadiLike(self.array * other)
 
     def __rmul__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides * operator"""
         if type(self) is type(other):
             return CasadiLike(self.array * other.array)
         else:
             return CasadiLike(self.array * other)
 
     def __add__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides + operator"""
         if type(self) is type(other):
             return CasadiLike(self.array + other.array)
         else:
             return CasadiLike(self.array + other)
 
     def __radd__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides + operator"""
         if type(self) is type(other):
             return CasadiLike(self.array + other.array)
         else:
             return CasadiLike(self.array + other)
 
     def __sub__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides - operator"""
         if type(self) is type(other):
             return CasadiLike(self.array - other.array)
         else:
             return CasadiLike(self.array - other)
 
     def __rsub__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides - operator"""
         if type(self) is type(other):
             return CasadiLike(self.array - other.array)
         else:
             return CasadiLike(self.array - other)
 
     def __neg__(self) -> "CasadiLike":
+        """Overrides - operator"""
         return CasadiLike(-self.array)
 
     def __truediv__(self, other: Union["CasadiLike", npt.ArrayLike]) -> "CasadiLike":
+        """Overrides / operator"""
         if type(self) is type(other):
             return CasadiLike(self.array / other.array)
         else:
             return CasadiLike(self.array / other)
 
     def __setitem__(self, idx, value: Union["CasadiLike", npt.ArrayLike]):
+        """Overrides set item operator"""
         self.array[idx] = value.array if type(self) is type(value) else value
 
     def __getitem__(self, idx) -> "CasadiLike":
+        """Overrides get item operator"""
         return CasadiLike(self.array[idx])
 
     @property
     def T(self) -> "CasadiLike":
+        """
+        Returns:
+            CasadiLike: Transpose of the array
+        """
         return CasadiLike(self.array.T)
 
     @staticmethod
