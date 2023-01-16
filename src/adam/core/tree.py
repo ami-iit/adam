5ee16e6a0874bb047b22d729872517f1cf775cc6
giulero <gl.giuseppelerario@gmail.com>
2023-01-16 14:12:51 +0100

    Add access methods

--- src/adam/core/tree.py
+++ src/adam/core/tree.py
@@ -1,7 +1,7 @@
 import dataclasses
 import logging
 import pathlib
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from prettytable import PrettyTable
 from urdf_parser_py.urdf import URDF, Joint, Link
@@ -19,6 +19,14 @@ class Node:
     def __hash__(self) -> int:
         return hash(self.name)
 
+    def get_elements(self) -> Tuple[Link, Joint, Link]:
+        """returns the node with its parent arc and parent link
+
+        Returns:
+            Tuple[Link, Joint, Link]: the node, the parent_arc, the parent_link
+        """
+        return self.link, self.parent_arc, self.parent
+
 
 @dataclasses.dataclass
 class Tree:
@@ -26,7 +34,7 @@ class Tree:
     root: str
 
     def __post_init__(self):
-        self.ordered_nodes_list = self.get_ordered_nodes_list()
+        self.ordered_nodes_list = self.get_ordered_nodes_list(self.root)
 
     @staticmethod
     def build_tree(links: List[Link], joints: List[Joint]) -> "Tree":
@@ -53,14 +61,41 @@ class Tree:
             raise ValueError("The model has more than one root link")
         return Tree(nodes, root_link[0])
 
-    def print(self) -> str:
+    # def add_node(self, new_node: Node) -> None:
+    #     self.graph[new_node.name] = new_node
+
+    #     if new_node.parent is not None:
+    #         self.graph[new_node.parent].children.append(new_node.link)
+
+    #     if new_node.parent_arc is not None:
+    #         self.graph[new_node.parent].arcs.append(new_node.parent_arc)
+    #         if new_node.parent_arc.child not in self.graph.keys():
+    #             self.graph[new_node.parent_arc.child] = Node(
+    #                 name=new_node.parent_arc.child,
+    #                 link=new_node.link,
+    #                 arcs=[],
+    #                 children=[],
+    #                 parent=new_node.parent,
+    #                 parent_arc=new_node.parent_arc,
+    #             )
+
+    #     for arc in new_node.arcs:
+    #         self.graph[arc.child].parent = new_node.link
+    #         self.graph[arc.child].parent_arc = arc
+
+    #     if self.root in [child.name for child in new_node.children]:
+    #         self.ordered_nodes_list = self.get_ordered_nodes_list(new_node.name)
+
+    #     print(self.ordered_nodes_list)
+
+    def print(self, root) -> str:
         import pptree
 
-        pptree.print_tree(self.graph[self.root])
+        pptree.print_tree(self.graph[root])
 
-    def get_ordered_nodes_list(self):
-        ordered_list = [self.root]
-        self.get_children(self.graph[self.root], ordered_list)
+    def get_ordered_nodes_list(self, start):
+        ordered_list = [start]
+        self.get_children(self.graph[start], ordered_list)
         return ordered_list
 
     @classmethod
@@ -75,14 +110,23 @@ class Tree:
                 list.append(child.name)
                 cls.get_children(child, list)
 
-    def __iter__(self):
+    def get_idx_from_name(self, name: str) -> int:
+        return self.ordered_nodes_list.index(name)
+
+    def get_name_from_idx(self, idx: int) -> str:
+        return self.ordered_nodes_list[idx]
+
+    def get_node_from_name(self, name: str) -> Node:
+        return self.graph[name]
+
+    def __iter__(self) -> Node:
         yield from [self.graph[name] for name in self.ordered_nodes_list]
 
-    def __reversed__(self):
+    def __reversed__(self) -> Node:
         yield from [self.graph[name] for name in reversed(self.ordered_nodes_list)]
 
-    def __getitem__(self, key):
-        return self.ordered_nodes_list[key]
+    def __getitem__(self, key) -> Node:
+        return self.graph[self.ordered_nodes_list[key]]
 
     def __len__(self) -> int:
         return len(self.ordered_nodes_list)
