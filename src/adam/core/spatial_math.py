f9e3d71efd55e9aa0a734b2b43d03729814b96d7
giulero <gl.giuseppelerario@gmail.com>
2023-06-09 10:49:03 +0200

    Add static decorator

--- src/adam/core/spatial_math.py
+++ src/adam/core/spatial_math.py
@@ -67,6 +67,7 @@ class ArrayLike(abc.ABC):
 class ArrayLikeFactory(abc.ABC):
     """Abstract class for a generic Array wrapper. Every method should be implemented for every data type."""
 
+    @staticmethod
     @abc.abstractmethod
     def zeros(x: npt.ArrayLike) -> npt.ArrayLike:
         """
@@ -78,6 +79,7 @@ class ArrayLikeFactory(abc.ABC):
         """
         pass
 
+    @staticmethod
     @abc.abstractmethod
     def eye(x: npt.ArrayLike) -> npt.ArrayLike:
         """
