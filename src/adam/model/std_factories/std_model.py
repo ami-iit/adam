349fbc649b334d6050e347c1502ea0b42638fdac
giulero <gl.giuseppelerario@gmail.com>
2024-01-12 13:13:55 +0100

    Align with black formatting

--- src/adam/model/std_factories/std_model.py
+++ src/adam/model/std_factories/std_model.py
@@ -7,11 +7,12 @@ import urdf_parser_py.urdf
 from adam.core.spatial_math import SpatialMath
 from adam.model import ModelFactory, StdJoint, StdLink
 
+
 def urdf_remove_sensors_tags(xml_string):
     # Parse the XML string
     root = ET.fromstring(xml_string)
 
-    # Find and remove all tags named "sensor" that are child of 
+    # Find and remove all tags named "sensor" that are child of
     # root node (i.e. robot)
     for sensors_tag in root.findall("sensor"):
         root.remove(sensors_tag)
@@ -20,6 +21,7 @@ def urdf_remove_sensors_tags(xml_string):
     modified_xml_string = ET.tostring(root)
 
     return modified_xml_string
+
 
 class URDFModelFactory(ModelFactory):
     """This factory generates robot elements from urdf_parser_py
@@ -36,17 +38,19 @@ class URDFModelFactory(ModelFactory):
             raise FileExistsError(path)
 
         # Read URDF, but before passing it to urdf_parser_py get rid of all sensor tags
-        # sensor tags are valid elements of URDF (see ), 
+        # sensor tags are valid elements of URDF (see ),
         # but they are ignored by urdf_parser_py, that complains every time it sees one.
         # As there is nothing to be fixed in the used models, and it is not useful
         # to have a useless and noisy warning, let's remove before hands all the sensor elements,
         # that anyhow are not parser by urdf_parser_py or adam
         # See https://github.com/ami-iit/ADAM/issues/59
-        xml_file = open(path, 'r')
+        xml_file = open(path, "r")
         xml_string = xml_file.read()
         xml_file.close()
         xml_string_without_sensors_tags = urdf_remove_sensors_tags(xml_string)
-        self.urdf_desc = urdf_parser_py.urdf.URDF.from_xml_string(xml_string_without_sensors_tags)
+        self.urdf_desc = urdf_parser_py.urdf.URDF.from_xml_string(
+            xml_string_without_sensors_tags
+        )
         self.name = self.urdf_desc.name
 
     def get_joints(self) -> List[StdJoint]:
