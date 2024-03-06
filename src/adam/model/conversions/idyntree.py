bc5f5eeb37de0b832c00a3f0fb35ad993d8858fc
Stefano <stefano.dafarra@gmail.com>
2024-03-06 11:43:43 +0100

    Completed draft of conversion to iDynTree model
    To be tested

--- src/adam/model/conversions/idyntree.py
+++ src/adam/model/conversions/idyntree.py
@@ -3,8 +3,10 @@ from idyntree.bindings import IJoint as idyn_joint
 from idyntree.bindings import Link as idyn_link
 from idyntree.bindings import Model as idyn_model
 from idyntree.bindings import SolidShape as idyn_solid_shape
+from idyntree.bindings import RotationalInertia as idyn_rotational_inertia
 import numpy as np
 import urdf_parser_py.urdf
+from typing import List
 
 
 from adam.model.model import Model
@@ -13,55 +15,58 @@ from adam.model.abc_factories import Link, Joint
 
 def to_idyntree_solid_shape(visuals: urdf_parser_py.urdf.Visual) -> idyn_solid_shape:
     """
-    Convert an urdf visual to an iDynTree solid shape
-    :param visuals: The input visual
-    :return: The iDynTree solid shape
+    Args:
+        visuals (urdf_parser_py.urdf.Visual): the visual to convert
+
+    Returns:
+        iDynTree.SolidShape: the iDynTree solid shape
     """
-    if type(visuals.geometry) is urdf_parser_py.urdf.Box:
+    if isinstance(visuals.geometry, urdf_parser_py.urdf.Box):
         output = idyntree.bindings.Box()
         output.setX(visuals.geometry.size[0])
         output.setY(visuals.geometry.size[1])
         output.setZ(visuals.geometry.size[2])
         return output
-    if type(visuals.geometry) is urdf_parser_py.urdf.Cylinder:
+    if isinstance(visuals.geometry, urdf_parser_py.urdf.Cylinder):
         output = idyntree.bindings.Cylinder()
         output.setRadius(visuals.geometry.radius)
         output.setLength(visuals.geometry.length)
         return output
 
-    if type(visuals.geometry) is urdf_parser_py.urdf.Sphere:
+    if isinstance(visuals.geometry, urdf_parser_py.urdf.Sphere):
         output = idyntree.bindings.Sphere()
         output.setRadius(visuals.geometry.radius)
         return output
-    if type(visuals.geometry) is urdf_parser_py.urdf.Mesh:
+    if isinstance(visuals.geometry, urdf_parser_py.urdf.Mesh):
         output = idyntree.bindings.ExternalMesh()
         output.setFilename(visuals.geometry.filename)
         output.setScale(visuals.geometry.scale)
         return output
 
-    raise NotImplementedError("The visual type is not supported")
+    raise NotImplementedError(
+        f"The visual type {visuals.geometry.__class__} is not supported"
+    )
 
 
-def to_idyntree_link(link: Link) -> [idyn_link, idyn_solid_shape]:
+def to_idyntree_link(link: Link) -> [idyn_link, List[idyn_solid_shape]]:
     """
     Args:
         link (Link): the link to convert
 
     Returns:
-        A tuple containing the iDynTree link and the iDynTree solid shape
+        A tuple containing the iDynTree link and the iDynTree solid shapes
     """
     output = idyn_link()
-    I = link.inertial.inertia
+    input_inertia = link.inertial.inertia
     inertia_matrix = np.array(
-        [[I.ixx, I.ixy, I.ixz], [I.ixy, I.iyy, I.iyz], [I.ixz, I.iyz, I.izz]]
+        [
+            [input_inertia.ixx, input_inertia.ixy, input_inertia.ixz],
+            [input_inertia.ixy, input_inertia.iyy, input_inertia.iyz],
+            [input_inertia.ixz, input_inertia.iyz, input_inertia.izz],
+        ]
     )
-    inertia_rotation = idyntree.bindings.Rotation.RPY(
-        link.inertial.origin.rpy[0],
-        link.inertial.origin.rpy[1],
-        link.inertial.origin.rpy[2],
-    )
-    idyn_spatial_rotational_inertia = idyntree.bindings.RotationalInertia()
-    idyn_spatial_rotational_inertia.FromPython(inertia_matrix)
+    inertia_rotation = idyntree.bindings.Rotation.RPY(*link.inertial.origin.rpy)
+    idyn_spatial_rotational_inertia = idyn_rotational_inertia.FromPython(inertia_matrix)
     rotated_inertia = inertia_rotation * idyn_spatial_rotational_inertia
     idyn_spatial_inertia = idyntree.bindings.SpatialInertia()
     idyn_spatial_inertia.fromRotationalInertiaWrtCenterOfMass(
@@ -69,19 +74,45 @@ def to_idyntree_link(link: Link) -> [idyn_link, idyn_solid_shape]:
     )
     output.setInertia(idyn_spatial_inertia)
 
-    # Here I need to convert the visual to an idyntree solid shape
-    pass
+    return output, [to_idyntree_solid_shape(v) for v in link.visuals]
 
 
-def to_idyntree_joint(joint: Joint) -> idyn_joint:
+def to_idyntree_joint(joint: Joint, parent_index: int, child_index: int) -> idyn_joint:
     """
     Args:
         joint (Joint): the joint to convert
-
+        parent_index (int): the parent link index
+        child_index (int): the child link index
     Returns:
         iDynTree.bindings.IJoint: the iDynTree joint
     """
-    pass
+
+    # TODO: consider limits
+
+    rest_position = idyntree.bindings.Position.FromPython(joint.origin.xyz)  # noqa
+    rest_rotation = idyntree.bindings.Rotation.RPY(*joint.origin.rpy)  # noqa
+    rest_transform = idyntree.bindings.Transform(rest_rotation, rest_position)
+
+    if joint.type == "fixed":
+        return idyntree.bindings.FixedJoint(parent_index, child_index, rest_transform)
+
+    direction = idyntree.bindings.Direction.FromPython(joint.axis)
+    axis = idyntree.bindings.Axis(direction, idyntree.bindings.Position.Zero())
+
+    if joint.type in ["revolute", "continuous"]:
+        output = idyntree.bindings.RevoluteJoint()
+        output.setAttachedLinks(parent_index, child_index)
+        output.setRestTransform(rest_transform)
+        output.setAxis(axis, child_index, parent_index)
+        return output
+    if joint.type in ["prismatic"]:
+        output = idyntree.bindings.PrismaticJoint()
+        output.setAttachedLinks(parent_index, child_index)
+        output.setRestTransform(rest_transform)
+        output.setAxis(axis, child_index, parent_index)
+        return output
+
+    NotImplementedError(f"The joint type {joint.type} is not supported")
 
 
 def to_idyntree_model(model: Model) -> idyn_model:
@@ -92,4 +123,25 @@ def to_idyntree_model(model: Model) -> idyn_model:
     Returns:
         iDynTree.Model: the iDynTree model
     """
-    pass
+    output = idyn_model()
+    output_visuals = []
+    links_map = {}
+    for node in model.tree:
+        link, visuals = to_idyntree_link(node.link)
+        link_index = output.addLink(node.name, link)
+        assert output.isValidLinkIndex(link_index)
+        assert link_index == len(output_visuals)
+        output_visuals.append(visuals)
+        links_map[node.name] = link_index
+
+    output.visualSolidShapes().resize(len(output_visuals))
+    for i, visuals in enumerate(output_visuals):
+        output.visualSolidShapes().set(i, visuals)
+
+    for node in model.tree:
+        for j in node.arcs:
+            joint = to_idyntree_joint(j, links_map[j.parent], links_map[j.child])
+            joint_index = output.addJoint(j.name, joint)
+            assert output.isValidJointIndex(joint_index)
+
+    return output
