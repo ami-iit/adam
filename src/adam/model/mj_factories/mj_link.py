880835c7bf5477e6cf268cfc104befddfa73e9e7
giulero <gl.giuseppelerario@gmail.com>
2024-07-17 18:34:44 +0200

    Remove function implementation since I moved them into the corresponding abc classes

--- src/adam/model/mj_factories/mj_link.py
+++ src/adam/model/mj_factories/mj_link.py
@@ -63,25 +63,3 @@ class MJLink(Link):
         """Transform the diagonal inertia tensor to the original frame."""
         I_diag = np.diag(diaginertia)
         return rotation_matrix @ I_diag @ rotation_matrix.T
-
-    def spatial_inertia(self) -> npt.ArrayLike:
-        """
-        Returns:
-            npt.ArrayLike: the 6x6 inertia matrix expressed at
-                           the origin of the link (with rotation)
-        """
-        I = self.inertial.inertia
-        mass = self.inertial.mass
-        o = self.inertial.origin.xyz
-        rpy = self.inertial.origin.rpy
-        return self.math.spatial_inertia(I, mass, o, rpy)
-
-    def homogeneous(self) -> npt.ArrayLike:
-        """
-        Returns:
-            npt.ArrayLike: the homogeneous transform of the link
-        """
-        return self.math.H_from_Pos_RPY(
-            self.inertial.origin.xyz,
-            self.inertial.origin.rpy,
-        )
