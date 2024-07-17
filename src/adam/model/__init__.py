ac510280920a490053674172b39138c8b1643693
giulero <gl.giuseppelerario@gmail.com>
2024-07-17 13:13:30 +0200

    Refactor model import in __init__.py to include MJLink, MJJoint, and MJModelFactory

--- src/adam/model/__init__.py
+++ src/adam/model/__init__.py
@@ -1,4 +1,7 @@
-from .abc_factories import Joint, Link, ModelFactory, Inertial, Pose
+from .abc_factories import Inertia, Inertial, Joint, Link, ModelFactory, Pose
+from .mj_factories.mj_joint import MJJoint
+from .mj_factories.mj_link import MJLink
+from .mj_factories.mj_model import MJModelFactory
 from .model import Model
 from .std_factories.std_joint import StdJoint
 from .std_factories.std_link import StdLink
