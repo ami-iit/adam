21b1a813f167a3ce7157d861405a4c603f175c59
giulero <gl.giuseppelerario@gmail.com>
2024-12-27 18:39:22 +0100

    Update copyright notices to remove year specification

--- src/adam/core/__init__.py
+++ src/adam/core/__init__.py
@@ -1,7 +1,6 @@
-# Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT). All rights reserved.
-# This software may be modified and distributed under the terms of the
-# GNU Lesser General Public License v2.1 or any later version.
+# Copyright (C) Istituto Italiano di Tecnologia (IIT). All rights reserved.
 
 from .constants import Representations
 from .rbd_algorithms import RBDAlgorithms
 from .spatial_math import SpatialMath
+from .kindyncomputations import KinDynComputationsBase
