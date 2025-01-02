ac697b8674af9bc81a95c27f373f01920391f2d7
giulero <gl.giuseppelerario@gmail.com>
2025-01-02 17:53:22 +0100

    Deprecate root_link parameter in pytorch KinDynComputationsParametric constructor and update documentation

--- src/adam/parametric/pytorch/computations_parametric.py
+++ src/adam/parametric/pytorch/computations_parametric.py
@@ -20,7 +20,7 @@ class KinDynComputationsParametric:
         urdfstring: str,
         joints_name_list: list,
         links_name_list: list,
-        root_link: str = "root_link",
+        root_link: str = None,
         gravity: np.array = torch.tensor(
             [0, 0, -9.80665, 0, 0, 0], dtype=torch.float64
         ),
@@ -30,7 +30,7 @@ class KinDynComputationsParametric:
             urdfstring (str): either path or string of the urdf
             joints_name_list (list): list of the actuated joints
             links_name_list (list): list of parametric links
-            root_link (str, optional): the first link. Defaults to 'root_link'.
+            root_link (str, optional): Deprecated. The root link is automatically chosen as the link with no parent in the URDF. Defaults to None.
         """
         self.math = SpatialMath()
         self.g = gravity
@@ -38,6 +38,10 @@ class KinDynComputationsParametric:
         self.joints_name_list = joints_name_list
         self.urdfstring = urdfstring
         self.representation = Representations.MIXED_REPRESENTATION  # Default
+        if root_link is not None:
+            raise DeprecationWarning(
+                "The root_link argument is not used. The root link is automatically chosen as the link with no parent in the URDF"
+            )
 
     def set_frame_velocity_representation(
         self, representation: Representations
