4df03a7bd4b8022b7d329ab1fc4bcf8f4849bf5c
giulero <gl.giuseppelerario@gmail.com>
2025-01-22 11:47:18 +0100

    Refactor NumpyLike and SpatialMath methods to eliminate type checks and streamline array handling

--- src/adam/numpy/numpy_like.py
+++ src/adam/numpy/numpy_like.py
@@ -18,10 +18,7 @@ class NumpyLike(ArrayLike):
 
     def __setitem__(self, idx, value: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides set item operator"""
-        if type(self) is type(value):
-            self.array[idx] = value.array.reshape(self.array[idx].shape)
-        else:
-            self.array[idx] = value
+        self.array[idx] = value.array.reshape(self.array[idx].shape)
 
     def __getitem__(self, idx) -> "NumpyLike":
         """Overrides get item operator"""
@@ -44,38 +41,23 @@ class NumpyLike(ArrayLike):
 
     def __matmul__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides @ operator"""
-        if type(self) is type(other):
-            return NumpyLike(self.array @ other.array)
-        else:
-            return NumpyLike(self.array @ np.array(other))
+        return NumpyLike(self.array @ other.array)
 
     def __rmatmul__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides @ operator"""
-        if type(self) is type(other):
-            return NumpyLike(other.array @ self.array)
-        else:
-            return NumpyLike(other @ self.array)
+        return NumpyLike(other.array @ self.array)
 
     def __mul__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides * operator"""
-        if type(self) is type(other):
-            return NumpyLike(self.array * other.array)
-        else:
-            return NumpyLike(self.array * other)
+        return NumpyLike(self.array * other.array)
 
     def __rmul__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides * operator"""
-        if type(self) is type(other):
-            return NumpyLike(other.array * self.array)
-        else:
-            return NumpyLike(other * self.array)
+        return NumpyLike(other.array * self.array)
 
     def __truediv__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides / operator"""
-        if type(self) is type(other):
-            return NumpyLike(self.array / other.array)
-        else:
-            return NumpyLike(self.array / other)
+        return NumpyLike(self.array / other.array)
 
     def __add__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides + operator"""
@@ -85,20 +67,14 @@ class NumpyLike(ArrayLike):
 
     def __radd__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides + operator"""
-        if type(self) is not type(other):
-            return NumpyLike(self.array + other)
         return NumpyLike(self.array + other.array)
 
     def __sub__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides - operator"""
-        if type(self) is not type(other):
-            return NumpyLike(self.array.squeeze() - other.squeeze())
-        return NumpyLike(self.array.squeeze() - other.array.squeeze())
+        return NumpyLike(self.array - other.array)
 
     def __rsub__(self, other: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
         """Overrides - operator"""
-        if type(self) is not type(other):
-            return NumpyLike(other.squeeze() - self.array.squeeze())
         return NumpyLike(other.array.squeeze() - self.array.squeeze())
 
     def __neg__(self):
@@ -132,6 +108,8 @@ class NumpyLikeFactory(ArrayLikeFactory):
         Returns:
             NumpyLike: Vector wrapping *x
         """
+        if not isinstance(x, (int, float, np.ndarray, list)):
+            raise ValueError(f"Cannot wrap {type(x)} in NumpyLike")
         return NumpyLike(np.array(x))
 
 
@@ -148,7 +126,7 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike: sin value of x
         """
-        return NumpyLike(np.sin(x))
+        return NumpyLike(np.sin(x.array))
 
     @staticmethod
     def cos(x: npt.ArrayLike) -> "NumpyLike":
@@ -159,7 +137,7 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike: cos value of x
         """
-        return NumpyLike(np.cos(x))
+        return NumpyLike(np.cos(x.array))
 
     @staticmethod
     def outer(x: npt.ArrayLike, y: npt.ArrayLike) -> "NumpyLike":
@@ -171,9 +149,7 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike: outer product of x and y
         """
-        x = np.array(x)
-        y = np.array(y)
-        return NumpyLike(np.outer(x, y))
+        return NumpyLike(np.outer(x.array, y.array))
 
     @staticmethod
     def vertcat(*x: Union["NumpyLike", npt.ArrayLike]) -> "NumpyLike":
@@ -181,10 +157,7 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike: vertical concatenation of x
         """
-        if isinstance(x[0], NumpyLike):
-            v = np.vstack([x[i].array for i in range(len(x))])
-        else:
-            v = np.vstack([x[i] for i in range(len(x))])
+        v = np.vstack([x[i].array for i in range(len(x))])
         return NumpyLike(v)
 
     @staticmethod
@@ -193,10 +166,7 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike: horrizontal concatenation of x
         """
-        if isinstance(x[0], NumpyLike):
-            v = np.hstack([x[i].array for i in range(len(x))])
-        else:
-            v = np.hstack([x[i] for i in range(len(x))])
+        v = np.hstack([x[i].array for i in range(len(x))])
         return NumpyLike(v)
 
     @staticmethod
@@ -208,7 +178,5 @@ class SpatialMath(SpatialMath):
         Returns:
             NumpyLike:  the skew symmetric matrix from x
         """
-        if not isinstance(x, NumpyLike):
-            return -np.cross(np.array(x), np.eye(3), axisa=0, axisb=0)
         x = x.array
         return NumpyLike(-np.cross(np.array(x), np.eye(3), axisa=0, axisb=0))
