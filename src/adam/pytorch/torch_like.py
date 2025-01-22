198fac656787fd6b4b538b12ff51fa50d0d225a4
giulero <gl.giuseppelerario@gmail.com>
2025-01-22 11:44:18 +0100

    Refactor TorchLike and SpatialMath methods to streamline array handling and eliminate type checks

--- src/adam/pytorch/torch_like.py
+++ src/adam/pytorch/torch_like.py
@@ -23,10 +23,7 @@ class TorchLike(ArrayLike):
 
     def __setitem__(self, idx, value: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides set item operator"""
-        if type(self) is type(value):
-            self.array[idx] = value.array.reshape(self.array[idx].shape)
-        else:
-            self.array[idx] = torch.tensor(value) if isinstance(value, float) else value
+        self.array[idx] = value.array.reshape(self.array[idx].shape)
 
     def __getitem__(self, idx):
         """Overrides get item operator"""
@@ -54,67 +51,39 @@ class TorchLike(ArrayLike):
 
     def __matmul__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides @ operator"""
-
-        if type(self) is type(other):
-            return TorchLike(self.array @ other.array)
-        if isinstance(other, torch.Tensor):
-            return TorchLike(self.array @ other)
-        else:
-            return TorchLike(self.array @ torch.tensor(other))
+        return TorchLike(self.array @ other.array)
 
     def __rmatmul__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides @ operator"""
-        if type(self) is type(other):
-            return TorchLike(other.array @ self.array)
-        else:
-            return TorchLike(torch.tensor(other) @ self.array)
+        return TorchLike(other.array @ self.array)
 
     def __mul__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides * operator"""
-        if type(self) is type(other):
-            return TorchLike(self.array * other.array)
-        else:
-            return TorchLike(self.array * other)
+        return TorchLike(self.array * other.array)
 
     def __rmul__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides * operator"""
-        if type(self) is type(other):
-            return TorchLike(other.array * self.array)
-        else:
-            return TorchLike(other * self.array)
+        return TorchLike(other.array * self.array)
 
     def __truediv__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides / operator"""
-        if type(self) is type(other):
-            return TorchLike(self.array / other.array)
-        else:
-            return TorchLike(self.array / other)
+        return TorchLike(self.array / other.array)
 
     def __add__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides + operator"""
-        if type(self) is not type(other):
-            return TorchLike(self.array.squeeze() + other.squeeze())
         return TorchLike(self.array.squeeze() + other.array.squeeze())
 
     def __radd__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides + operator"""
-        if type(self) is not type(other):
-            return TorchLike(self.array.squeeze() + other.squeeze())
         return TorchLike(self.array.squeeze() + other.array.squeeze())
 
     def __sub__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides - operator"""
-        if type(self) is type(other):
-            return TorchLike(self.array.squeeze() - other.array.squeeze())
-        else:
-            return TorchLike(self.array.squeeze() - other.squeeze())
+        return TorchLike(self.array.squeeze() - other.array.squeeze())
 
     def __rsub__(self, other: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
         """Overrides - operator"""
-        if type(self) is type(other):
-            return TorchLike(other.array.squeeze() - self.array.squeeze())
-        else:
-            return TorchLike(other.squeeze() - self.array.squeeze())
+        return TorchLike(other.array.squeeze() - self.array.squeeze())
 
     def __neg__(self) -> "TorchLike":
         """Overrides - operator"""
@@ -163,9 +132,7 @@ class SpatialMath(SpatialMath):
         Returns:
             TorchLike: sin value of x
         """
-        if isinstance(x, float):
-            x = torch.tensor(x)
-        return TorchLike(torch.sin(x))
+        return TorchLike(torch.sin(x.array))
 
     @staticmethod
     def cos(x: ntp.ArrayLike) -> "TorchLike":
@@ -177,9 +144,7 @@ class SpatialMath(SpatialMath):
             TorchLike: cos value of x
         """
         # transform to torch tensor, if not already
-        if isinstance(x, float):
-            x = torch.tensor(x)
-        return TorchLike(torch.cos(x))
+        return TorchLike(torch.cos(x.array))
 
     @staticmethod
     def outer(x: ntp.ArrayLike, y: ntp.ArrayLike) -> "TorchLike":
@@ -191,7 +156,7 @@ class SpatialMath(SpatialMath):
         Returns:
             TorchLike: outer product of x and y
         """
-        return TorchLike(torch.outer(torch.tensor(x), torch.tensor(y)))
+        return TorchLike(torch.outer(x.array, y.array))
 
     @staticmethod
     def skew(x: Union["TorchLike", ntp.ArrayLike]) -> "TorchLike":
@@ -202,10 +167,6 @@ class SpatialMath(SpatialMath):
         Returns:
             TorchLike: skew matrix from x
         """
-        if not isinstance(x, TorchLike):
-            return TorchLike(
-                torch.tensor([[0, -x[2], x[1]], [x[2], 0, -x[0]], [-x[1], x[0], 0]])
-            )
         x = x.array
         return TorchLike(
             torch.tensor([[0, -x[2], x[1]], [x[2], 0, -x[0]], [-x[1], x[0], 0]])
@@ -217,10 +178,7 @@ class SpatialMath(SpatialMath):
         Returns:
             TorchLike: vertical concatenation of x
         """
-        if isinstance(x[0], TorchLike):
-            v = torch.vstack([x[i].array for i in range(len(x))])
-        else:
-            v = torch.tensor(x)
+        v = torch.vstack([x[i].array for i in range(len(x))])
         return TorchLike(v)
 
     @staticmethod
@@ -229,8 +187,5 @@ class SpatialMath(SpatialMath):
         Returns:
             TorchLike: horizontal concatenation of x
         """
-        if isinstance(x[0], TorchLike):
-            v = torch.hstack([x[i].array for i in range(len(x))])
-        else:
-            v = torch.tensor(x)
+        v = torch.hstack([x[i].array for i in range(len(x))])
         return TorchLike(v)
