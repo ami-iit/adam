dd31e3a73d756f6f6c6d595a679ded154df79e5e
giulero <gl.giuseppelerario@gmail.com>
2025-01-22 11:42:38 +0100

    Update usual copyright information

--- src/adam/pytorch/computation_batch.py
+++ src/adam/pytorch/computation_batch.py
@@ -1,6 +1,4 @@
-# Copyright (C) 2024 Istituto Italiano di Tecnologia (IIT). All rights reserved.
-# This software may be modified and distributed under the terms of the
-# GNU Lesser General Public License v2.1 or any later version.
+# Copyright (C) Istituto Italiano di Tecnologia (IIT). All rights reserved.
 
 import warnings
 
